c14d4de411fe6be903cf9e934b7d8f9bb36ce8cb
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-13 17:44:03 +0100

    Created validator function for empty values

---
+++ validation/isEmpty.js
@@ -0,0 +1,9 @@
+// Checks if a given value is empty or not
+// Returns true if value is empty and false if it isn't
+const isEmpty = (value) =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+module.exports = isEmpty;
