0518788431289cbb23523668f7ffd709e9203239
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-18 18:20:14 +0100

    Created validation for adding experience

---
+++ validation/experience.js
@@ -0,0 +1,34 @@
+const validator = require("validator");
+const isEmpty = require("./isEmpty");
+
+module.exports = function validateExperienceInput(data) {
+  let errors = {};
+
+  data.title = !isEmpty(data.title) ? data.title : "";
+  data.company = !isEmpty(data.company) ? data.company : "";
+  data.from = !isEmpty(data.from) ? data.from : "";
+
+  // Title
+  if (validator.isEmpty(data.title)) {
+    errors.title = "Title field is required";
+  }
+
+  // Company
+  if (validator.isEmpty(data.company)) {
+    errors.company = "Company field is required";
+  }
+
+  // Date
+  // if (!validator.isDate(date.from)) {
+  //   errors.from = "Please enter a valid date";
+  // }
+
+  if (validator.isEmpty(data.from)) {
+    errors.from = "Title field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
